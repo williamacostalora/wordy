a03f831b54fe0356af7996586a87bcaf644eeac1
williamacostalora <157067981+williamacostalora@users.noreply.github.com>
2026-09-21 11:04:41 -0500

    fix: implemented compile for constant node, tests pass

--- src/wordy/ast/ConstantNode.java
+++ src/wordy/ast/ConstantNode.java
@@ -1,5 +1,6 @@
 package wordy.ast;
 
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
@@ -48,5 +49,8 @@ public final class ConstantNode extends ExpressionNode {
 
     @Override
     protected double doEvaluate(EvaluationContext context) { return value ; }
+
+    @Override
+    public void compile(PrintWriter out){ out.print(value);}
 }
 
