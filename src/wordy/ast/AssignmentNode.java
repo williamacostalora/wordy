b4e9a307c783ad624183375161f4709a5ba85b3b
williamacostalora <157067981+williamacostalora@users.noreply.github.com>
2026-09-22 11:32:02 -0500

    fix:implemented compile for assignment node, tests pass

--- src/wordy/ast/AssignmentNode.java
+++ src/wordy/ast/AssignmentNode.java
@@ -2,6 +2,7 @@ package wordy.ast;
 
 import wordy.interpreter.EvaluationContext;
 
+import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Objects;
 
@@ -60,5 +61,13 @@ public class AssignmentNode extends StatementNode {
     @Override
     protected void doRun(EvaluationContext context) {
         context.set(variable.getName(), expression.evaluate(context));
+    }
+
+    @Override
+    public void compile(PrintWriter out) {
+        variable.compile(out);
+        out.print("=");
+        expression.compile(out);
+        out.print(";");
     }
 }
