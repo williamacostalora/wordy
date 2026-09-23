7499633f964ee7e925efbe07df35859b4d296e62
williamacostalora <157067981+williamacostalora@users.noreply.github.com>
2026-09-22 23:55:35 -0500

    fix:implemented loop exit compile, tests pass

--- src/wordy/ast/LoopExitNode.java
+++ src/wordy/ast/LoopExitNode.java
@@ -3,6 +3,7 @@ package wordy.ast;
 import wordy.interpreter.EvaluationContext;
 import wordy.interpreter.LoopExited;
 
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Map;
 
@@ -39,5 +40,10 @@ public final class LoopExitNode extends StatementNode {
     @Override
     protected void doRun(EvaluationContext context) {
         throw new LoopExited();
+    }
+
+    @Override
+    public void compile(PrintWriter out){
+        out.println("break;");
     }
 }
